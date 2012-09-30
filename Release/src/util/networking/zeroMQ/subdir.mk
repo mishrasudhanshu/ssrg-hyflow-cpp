ce33e5ef50d41d2ab1d39a035554f553e32733a0
sudhanshu.knit <sudhanshu.knit@localhost>
2012-09-30 01:48:29 -0400

    Make file update for ZeroMQ

---
+++ Release/src/util/networking/zeroMQ/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/util/networking/zeroMQ/ZMQNetwork.cpp 
+
+OBJS += \
+./src/util/networking/zeroMQ/ZMQNetwork.o 
+
+CPP_DEPS += \
+./src/util/networking/zeroMQ/ZMQNetwork.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/util/networking/zeroMQ/%.o: ../src/util/networking/zeroMQ/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -DRELEASE -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
